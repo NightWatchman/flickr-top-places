537532a02719e969aed6de4fc0858f154f67d464
Eric Rushing <rushinge@gmail.com>
2013-02-26 14:16:49 -0800

    Initial Commit

---
+++ FlickrTopPlaces/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  FlickrTopPlaces
+//
+//  Created by Eric Rushing on 2/26/13.
+//  Copyright (c) 2013 Maps.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
