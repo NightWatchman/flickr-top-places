dd01dafe7a439dc7189373d290ffc3cead71c901
Eric Rushing <rushinge@gmail.com>
2013-03-04 23:02:34 -0800

    PlacePhotosViewController download photos list in separate thread

--- FlickrTopPlaces/PlacePhotosViewController.m
+++ FlickrTopPlaces/PlacePhotosViewController.m
@@ -5,20 +5,46 @@
 
 @interface PlacePhotosViewController ()
 
+@property (nonatomic) dispatch_queue_t downloadQueue;
+
 @end
 
 
 @implementation PlacePhotosViewController
 
 @synthesize place = _place;
-
-- (void)viewDidLoad
-{
-  [super viewDidLoad];
-  NSDictionary *placeName = [FlickrPlaceUtil nameForPlace:self.place];
+- (void)setPlace:(NSDictionary *)place {
+  _place = place;
+  NSDictionary *placeName = [FlickrPlaceUtil
+                             nameForPlace:self.place];
   NSString *city = [placeName objectForKey:FLICKR_PLACE_NAME_CITY_KEY];
   self.title = [city stringByAppendingString:@" Photos"];
-  self.photos = [FlickrFetcher photosInPlace:self.place maxResults:50];
+  dispatch_async(self.downloadQueue, ^{
+    NSArray *photos = [FlickrFetcher photosInPlace:self.place maxResults:50];
+    dispatch_async(dispatch_get_main_queue(), ^{
+      self.photos = photos;
+    });
+  });
+}
+
+@synthesize downloadQueue = _downloadQueue;
+- (dispatch_queue_t)downloadQueue
+{
+  if (!_downloadQueue)
+    _downloadQueue = dispatch_queue_create("Place Photos Download", nil);
+  return _downloadQueue;
+}
+
+- (void)viewDidUnload
+{
+  [super viewDidUnload];
+  dispatch_release(self.downloadQueue);
+}
+
+- (void)didReceiveMemoryWarning
+{
+  [super didReceiveMemoryWarning];
+  dispatch_release(self.downloadQueue);
 }
 
 @end
