996d16723001c8c6896ea2ad7518c50686d6b8e3
Eric Rushing <rushinge@gmail.com>
2013-03-06 16:13:50 -0800

    Support all orientations

---
+++ FlickrTopPlaces/FlickrTopPlacesRootViewController.m
@@ -0,0 +1,26 @@
+#import "FlickrTopPlacesRootViewController.h"
+
+
+@interface FlickrTopPlacesRootViewController ()
+
+@end
+
+
+@implementation FlickrTopPlacesRootViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+  self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+  if (self) {
+    // Custom initialization
+  }
+  return self;
+}
+
+-(BOOL)shouldAutorotateToInterfaceOrientation:
+(UIInterfaceOrientation)toInterfaceOrientation
+{
+  return YES;
+}
+
+@end
